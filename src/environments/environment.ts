781eeb599c08facdafa03afac385786b1c0efdd5
Óscar Rivera <78364849+ORiveraEPN@users.noreply.github.com>
2021-03-14 18:25:11 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 export const environment = {
   "production": false,
-  "url": "http://loadb-app-335908590.us-east-1.elb.amazonaws.com",
+  "url": "http://loadb-app-2074917841.us-east-1.elb.amazonaws.com",
   "port": ""
 };
